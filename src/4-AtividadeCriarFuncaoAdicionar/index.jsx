bccf5366cc0b09b5e1335d77f0191cdaf73044fc
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-07-31 23:24:48 -0300

    Update index.jsx

--- src/4-AtividadeCriarFuncaoAdicionar/index.jsx
+++ src/4-AtividadeCriarFuncaoAdicionar/index.jsx
@@ -1,16 +1,34 @@
-// Crie o estado `listaPedidos`.
-// Dentro do componente, crie a função `adicionarItemPedidos`.
-// A função recebe um parâmetro, que pode ser nomeado como `objeto`.
-// Dentro da função, utilize `setListaPedidos` para adicionar o `objeto` à `listaPedidos`.
-// Exemplo: `setListaPedidos([...listaPedidos, objeto]);`
-
+import { useState } from "react";
 
 export default function Home() {
- 
+
+  const [listaProdutos, setProdutos] = useState([
+    { id: 1, nome: 'Flatwoods Monster: Além de Fallout 76', preço: 'R$ 19,52' },
+    { id: 2, nome: 'Indrid Cold: O Humanóide de Lanulos', preço: 'R$ 19,66' },
+    { id: 3, nome: 'Mothman: As Profecias', preço: 'R$ 19,66' },
+    { id: 4, nome: 'Owlman: O Mothman Britânico', preço: 'R$ 19,76' },
+    { id: 5, nome: 'Melon Heads: Canibais Americanos', preço: 'R$ 19,60' },
+    { id: 6, nome: 'Criptozoologia: Guia Completo', preço: 'R$ 19,53' }
+  ]);
+
+  const [listaPedidos, setListaPedidos] = useState([]);
+
+  const adicionarItemPedidos = (objeto) => {
+    setListaPedidos([...listaPedidos, objeto])
+  }
+
   return (
     <div>
-        
+      <h1>Livros sobre Criptídeos</h1>
+
+      {
+        listaProdutos.map((produto) =>
+          <div key={produto.id}>
+            <p>{produto.nome}</p>
+            <p>{produto.preço}</p>
+          </div>
+        )
+      }
     </div>
-   
   );
 }
