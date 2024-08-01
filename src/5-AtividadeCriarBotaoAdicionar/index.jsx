e8e44d69c6389d61e047bc81ada53ad539533d25
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-07-31 23:24:28 -0300

    Update index.jsx

--- src/5-AtividadeCriarBotaoAdicionar/index.jsx
+++ src/5-AtividadeCriarBotaoAdicionar/index.jsx
@@ -1,16 +1,35 @@
-// Para cada objeto renderizado, adicione um botão.
-// Utilize o atributo onClick para o botão.
-// No valor do onClick, passe a função adicionarItemPedidos, 
-// por exemplo: onClick={() => adicionarItemPedidos(produto)}
-// No conteúdo do botão, coloque o texto "Selecionar" ou algo semelhante.
-
 import { useState } from "react";
 
 export default function Home() {
+
+  const [listaProdutos, setProdutos] = useState([
+    { id: 1, nome: 'Flatwoods Monster: Além de Fallout 76', preço: 'R$ 19,52' },
+    { id: 2, nome: 'Indrid Cold: O Humanóide de Lanulos', preço: 'R$ 19,66' },
+    { id: 3, nome: 'Mothman: As Profecias', preço: 'R$ 19,66' },
+    { id: 4, nome: 'Owlman: O Mothman Britânico', preço: 'R$ 19,76' },
+    { id: 5, nome: 'Melon Heads: Canibais Americanos', preço: 'R$ 19,60' },
+    { id: 6, nome: 'Criptozoologia: Guia Completo', preço: 'R$ 19,53' }
+  ]);
+
+  const [listaPedidos, setListaPedidos] = useState([]);
+
+  const adicionarItemPedidos = (objeto) => {
+    setListaPedidos([...listaPedidos, objeto])
+  }
+
   return (
     <div>
-       
+      <h1>Livros sobre Criptídeos</h1>
+
+      {
+        listaProdutos.map((produto) =>
+          <div key={produto.id}>
+            <p>{produto.nome}</p>
+            <p>{produto.preço}</p>
+            <button onClick={() => adicionarItemPedidos(produto)}>Selecionar</button>
+          </div>
+        )
+      }
     </div>
-   
   );
 }
