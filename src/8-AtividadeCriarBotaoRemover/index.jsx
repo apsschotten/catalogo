c2a22f8229bd565c29fccd3cec8d534f178a6932
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-07-31 23:23:22 -0300

    Update index.jsx

--- src/8-AtividadeCriarBotaoRemover/index.jsx
+++ src/8-AtividadeCriarBotaoRemover/index.jsx
@@ -1,16 +1,61 @@
-// Dentro do `map`, crie um botão para cada item.
-// Utilize o atributo `onClick` para definir a ação do botão.
-// No valor do `onClick`, chame a função `removerItemPedidos`.
-// Passe o identificador (`id`) do item como parâmetro para `removerItemPedidos`, o mesmo que você usa no `map`.
-// No conteúdo do botão, coloque o texto "Remover".
-
 import { useState } from "react";
 
 export default function Home() {
 
+  const [listaProdutos, setProdutos] = useState([
+    { id: 1, nome: 'Flatwoods Monster: Além de Fallout 76', preço: 'R$ 19,52' },
+    { id: 2, nome: 'Indrid Cold: O Humanóide de Lanulos', preço: 'R$ 19,66' },
+    { id: 3, nome: 'Mothman: As Profecias', preço: 'R$ 19,66' },
+    { id: 4, nome: 'Owlman: O Mothman Britânico', preço: 'R$ 19,76' },
+    { id: 5, nome: 'Melon Heads: Canibais Americanos', preço: 'R$ 19,60' },
+    { id: 6, nome: 'Criptozoologia: Guia Completo', preço: 'R$ 19,53' }
+  ]);
+
+  const [listaPedidos, setListaPedidos] = useState([]);
+
+  const adicionarItemPedidos = (objeto) => {
+    setListaPedidos([...listaPedidos, objeto])
+  }
+
+  const removerPedido = (id) => {
+    let remover = false;
+    let listaAux = listaPedidos.filter((produto) => {
+      if (remover == false) {
+        if (produto.id !== id) {
+          return produto
+        } else {
+          remover = true;
+          return null
+        }
+      } else {
+        return produto
+      }
+    });
+    setListaPedidos(listaAux);
+  }
+
   return (
     <div>
-         
+      <h1>Livros sobre Criptídeos</h1>
+
+      {
+        listaProdutos.map((produto) =>
+          <div key={produto.id}>
+            <p>{produto.nome}</p>
+            <p>{produto.preço}</p>
+            <button onClick={() => adicionarItemPedidos(produto)}>Selecionar</button>
+          </div>
+        )
+      }
+      {
+        listaPedidos.map((produto) =>
+          <div key={produto.id}>
+            <p>{produto.nome}</p>
+            <p>{produto.preço}</p>
+            <button onClick={() => removerPedido(produto.id)}>Remover</button>
+          </div>
+        )
+      }
     </div>
   );
 }
