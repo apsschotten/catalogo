68309df7bc267989176fb74b40bafce35fe692cd
apsschotten <161073410+apsschotten@users.noreply.github.com>
2024-07-31 23:25:04 -0300

    Update index.jsx

--- src/3-AtividadeListarObjetos/index.jsx
+++ src/3-AtividadeListarObjetos/index.jsx
@@ -1,13 +1,28 @@
-// Crie um elemento `<div>`.
-// Utilize o método `map` para listar cada objeto da `listaProdutos`
-// dentro desse `<div>`.
+import { useState } from "react";
 
 export default function Home() {
 
+  const [listaProdutos, setProdutos] = useState([
+    { id: 1, nome: 'Flatwoods Monster: Além de Fallout 76', preço: 'R$ 19,52' },
+    { id: 2, nome: 'Indrid Cold: O Humanóide de Lanulos', preço: 'R$ 19,66' },
+    { id: 3, nome: 'Mothman: As Profecias', preço: 'R$ 19,66' },
+    { id: 4, nome: 'Owlman: O Mothman Britânico', preço: 'R$ 19,76' },
+    { id: 5, nome: 'Melon Heads: Canibais Americanos', preço: 'R$ 19,60' },
+    { id: 6, nome: 'Criptozoologia: Guia Completo', preço: 'R$ 19,53' }
+  ]);
+
   return (
     <div>
-        
+      <h1>Livros sobre Criptídeos</h1>
+
+      {
+        listaProdutos.map((produto) =>
+          <div key={produto.id}>
+            <p>{produto.nome}</p>
+            <p>{produto.preço}</p>
+          </div>
+        )
+      }
     </div>
-   
   );
 }
